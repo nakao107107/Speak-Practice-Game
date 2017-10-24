05b1af34b2b1e1426b5b6e5ec8d5fd68dc50592f
M-dove <m.rynk.plume@gmail.com>
2017-10-24 20:33:26 +0900

    文字を受け取って評価配列を返すメソッドを実装

--- app/src/main/java/com/example/nakao/speakpracticegame/ArrayUtil.java
+++ app/src/main/java/com/example/nakao/speakpracticegame/ArrayUtil.java
@@ -1,14 +1,19 @@
 package com.example.nakao.speakpracticegame;
 
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by nakao on 2017/10/22.
  */
 
 public class ArrayUtil {
+
+    private static String GOJUON = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+
 
     boolean judgeArray(String word, ArrayList<String> array){
 
@@ -26,5 +31,40 @@ public class ArrayUtil {
 
         return judge;
 
+    }
+
+    public static int[] correctWord(String right_word, String sample_word,@Nullable int[] eval_array) {
+        if(eval_array == null){
+            int[] jarray = new int[50];
+        }
+        int[] jarray = eval_array;
+        Arrays.fill(jarray,0);
+        int p = 0;
+
+        char[] rascii = right_word.toCharArray();
+        char[] sascii = sample_word.toCharArray();
+
+        int i=0;
+        while (i<rascii.length && i<sascii.length){
+            p= GOJUON.indexOf(rascii[i]) ;
+            Log.d("TAG",String.valueOf(p));
+            if(p<0 || p>50) {
+                i++;
+                continue;
+            }
+
+            if(rascii[i] == sascii[i]){
+                jarray[p] += 1;
+            } else {
+                jarray[p] -= 1;
+            }
+
+            i++;
+        }
+        return  jarray;
+    }
+
+    public static int[] correctWord(String right_word, String sample_word){
+        return correctWord(right_word, sample_word, null);
     }
 }
