c17f2aaf418320ebbac6907a74d774a97dfa2303
Fumiaki Nakao <imakeanewstory@gmail.com>
2017-11-21 09:52:12 +0900

    タイトル画面の簡略化、ModeSelectActivityの追加

--- app/src/main/java/com/example/nakao/speakpracticegame/ModeSelectActivity.java
+++ app/src/main/java/com/example/nakao/speakpracticegame/ModeSelectActivity.java
@@ -1,57 +1,44 @@
 package com.example.nakao.speakpracticegame;
-
-import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
-
 /**
- * Created by nakao on 2017/06/18.
+ * Created by nakao on 2017/11/21.
  */
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-    Button GameStartButton;
-    Button SettingButton;
-    ImageView image;
+import com.example.nakao.speakpracticegame.R;
+
+
+public class ModeSelectActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button GameStartButton,SettingButton;
     LinearLayout linearlayout;
-    int currentlevel;
+    SharedPreferences process;
 
+    int currentlevel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_modeselectgi);
 
-        image=(ImageView)findViewById(R.id.birdgif);
-        GlideDrawableImageViewTarget target = new GlideDrawableImageViewTarget(image);
-        Glide.with(this).load(R.drawable.ordinary).into(target);
-
+        GameStartButton = (Button)findViewById(R.id.button1);
+        SettingButton = (Button) findViewById(R.id.button2);
         linearlayout=(LinearLayout)findViewById(R.id.background);
 
+        GameStartButton.setOnClickListener(this);
+        SettingButton.setOnClickListener(this);
 
         //Preferenceファイルから現在の最高クリアレベルを取得
-        SharedPreferences process = getSharedPreferences("level", Context.MODE_PRIVATE);
+        process = getSharedPreferences("level", Context.MODE_PRIVATE);
         currentlevel = process.getInt("currentlevel",0 );
-
 
         switch (currentlevel){
             case 0:
@@ -74,18 +61,21 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-        linearlayout.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                Intent intent=new Intent(v.getContext(),ModeSelectActivity.class);
-                startActivity(intent);
-                return false;
-            }
-        });
-
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.button1:
+                Intent intent=new Intent(v.getContext(),StageSelectActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                Intent intent2=new Intent(v.getContext(),SettingActivity.class);
+                startActivity(intent2);
+                break;
+        }
+
+    }
 }
-
-
