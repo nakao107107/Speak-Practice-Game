093b90aba69f66c34c060e381c1096f82574b247
M-dove <m.rynk.plume@gmail.com>
2017-11-22 22:36:46 +0900

    TokenizerUtilをKuroPredictStrategyに統合

--- app/src/main/java/com/example/nakao/speakpracticegame/KuroPredictStrategy.java
+++ app/src/main/java/com/example/nakao/speakpracticegame/KuroPredictStrategy.java
@@ -3,6 +3,8 @@ package com.example.nakao.speakpracticegame;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.atilika.kuromoji.ipadic.Token;
+import com.atilika.kuromoji.ipadic.Tokenizer;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
@@ -16,11 +18,34 @@ public class KuroPredictStrategy implements PredictStrategy {
 
     @Override
     public String predict(ArrayList<String> arrayList) {
-        TokenizerUtil tokenizerUtil = new TokenizerUtil();
+
         //kuromojiにかけてカタカナ変換
-        String tmp_string =tokenizerUtil.getKatakana(arrayList.get(0));
+        String tmp_string = getKatakana(arrayList.get(0));
 
         Log.d("進捗","kuromojiの処理が完了しました"+tmp_string);
         return tmp_string;
     }
+
+    public String getKatakana(String word) {
+
+        Tokenizer mTokenizer;
+        mTokenizer= new Tokenizer.Builder().build();
+        StringBuilder sb=new StringBuilder();
+
+
+        for (Token token : mTokenizer.tokenize(word)) {
+
+            String feature=token.getReading();
+            Log.d("getAllFeatureArrayの長さ", feature);
+
+            sb.append(feature);
+        }
+        return sb.toString();
+    }
 }
+
+
+
+
+
+
